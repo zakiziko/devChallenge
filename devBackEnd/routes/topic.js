4f0acc3fc66525b0b2958a6ea820870546db29a2
zakiziko <zakariaelmessoudi@gmail.com>
2017-12-23 10:54:35 +0000

    Updating backEnd
    1 Deleting useless function of getting topics by userId
    2 updating the return message from upvote route to be more usefull in frontend

--- devBackEnd/routes/topic.js
+++ devBackEnd/routes/topic.js
@@ -23,18 +23,6 @@ router.get('/all',(req,res)=>{
 })
 
 /**
- * get All topics Created By a User
- *@param {String} id
- *@return {json}
- */
-router.get('/creatorTopics/:id',(req,res)=>{
-  Topic.find({creator : req.params.id},function(err,topics){
-    if(err) return res.json('err : fetching data for topics');
-    res.json(topics);
-  });
-})
-
-/**
  * create a new tpoic based on json object passed in using body parser
  *@return {json}
  */
@@ -55,7 +43,7 @@ router.post('/add',(req,res)=>{
 /**
  * updating the upVote list on a specifique topic
  *@param {String} id topic ID
- *@return {json} message 
+ *@return {json} message
  */
 router.put('/upVote/:id',(req,res)=>{
   const query = { _id: req.params.id };
@@ -64,15 +52,15 @@ router.put('/upVote/:id',(req,res)=>{
   });
   Topic.update(query,{ $addToSet: { upVote: UpVoteUser }},
     function(err,callback){
-      let message = 'Up Voted successful';
+      let message = {state:true,msg:'seccess'};
       if(err){
-        message = 'something went wrong with your topic Id';
+        message = {state:false,msg:'something went wrong with your topic Id'};
       }else{
         if(callback.nModified === 0){
-          message = 'You Already UpVote this Topic';
+          message =  {state:false,msg:'You Already UpVote this Topic'};
         }
       }
-      res.json({'message' : message});
+      res.json(message);
     });
 
 });
